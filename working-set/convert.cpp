fc1dbfc24c86174b7f82b3068c11c4495fa2d65e
Yu-wen Pwu <ywpu@cs.nctu.edu.tw>
2016-12-10 03:04:28 +0800

    Finished convert program.

---
+++ working-set/convert.cpp
@@ -0,0 +1,30 @@
+#include <sys/stat.h>
+#include <fstream>
+
+int main(int argc, char** argv) {
+	std::ifstream in("data.txt", std::ios::in | std::ios::binary);
+	std::ofstream out("new_data.txt", std::ios::out | std::ios::trunc | std::ios::binary);
+
+	struct stat st;
+	stat("data.txt", &st);
+	int len = (int) st.st_size / 4096 * 4;
+	out.write((char *) &len, sizeof(int));
+
+	char buf[4096];
+	while (in.good()) {
+		in.read(buf, 4096);
+		out.write(buf, 4);
+	}
+
+	in.clear();
+	in.seekg(0);
+
+	while (in.good()) {
+		in.read(buf, 4096);
+		out.write(buf, 4096);
+	}
+
+	out.close();
+	in.close();
+	return 0;
+}
